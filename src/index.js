040b933214e3dd6ab2c3669ef9bf9cdedf876d1a
silvanu <silvan.usleber@webanizer.de>
2022-05-02 12:21:01 +0200

    Performance test: First attempt of multiple "put" into Docstore DB.

--- src/index.js
+++ src/index.js
@@ -1,90 +1,68 @@
-const IPFS = require('ipfs')
-const OrbitDB = require('orbit-db')
+const IPFS = require('ipfs');
+const OrbitDB = require('orbit-db');
 
-async function main() {
+async function main()
+{
 
-  // Create IPFS instance
-  const ipfsOptions = { repo: './ipfs', }
-  const ipfs = await IPFS.create(ipfsOptions)
+    // Create IPFS instance
+    const ipfsOptions = { repo: './ipfs', }
+    const ipfs = await IPFS.create(ipfsOptions);
 
-  // Create OrbitDB instance
-  const orbitdb = await OrbitDB.createInstance(ipfs)
+    // Create OrbitDB instance
+    const orbitDb = await OrbitDB.createInstance(ipfs);
 
-  /* 
-  By default and if not specified otherwise, only the creator of the database will be given write-access.
-  Access rights are setup by passing an accessController object that specifies 
-  the access-controller type and access rights of the database when created. 
-  OrbitDB currently supports write-access. 
-  The access rights are specified as an array of public keys of the peers who can write to the database. 
-  The public keys to which access is given can be retrieved from the identity.publicKey property of each peer.
+    // Create docstore DB
+    const docstore = await orbitDb.docstore('docstoreDB');
 
-  // Give write permission to following peers:
-  const options = {
-    // Give write access to ourselves
-    accessController: {
-      write: [orbitdb.identity.id]
+    // Set variables
+    const amount = 2000; // Amout of data to put
+    const timestamp = new Date().getTime(); // Timestamp (Only set once so we don't affect performance too much)
+    const timeStart = performance.now(); // Start time of the data loop
+
+    console.log("Starting to put data into Docstore DB...");
+
+    // Loop for our amount of data to put
+    for (let i = 0; i < amount; i++)
+    {
+        // Pseudo ID from timestamp + index should be enough for testing
+        let pseudoId = timestamp + i;
+
+        // Random KWh values between 1.000 and 100.000
+        let kwhValue = Math.floor(Math.random() * 100000) + 1000;
+
+        // Put line to DB
+        let result = await docstore.put({ _id: pseudoId, timestamp: timestamp, "1-0:1.8.0*255": kwhValue,  pin: true});
+
+        // Log every 100th iteration
+        if(i > 1 && i % 100 == 0)
+        {
+            console.log("Now: " + i + " of " + amount);
+        }
     }
-  }
 
-  // Create a public database that everyone can write to:
-  
-  write: ['*']
-  
-  */
+    const timeEnd = performance.now(); // End time of data loop
+    const timeDiff = (timeEnd - timeStart) / 1000; // Time difference in seconds
 
-// Database Type keyvalue
-  const db = await orbitdb.keyvalue('first-database')
+    console.log("Finished!");
+    console.log(amount + " put into DB in " + timeDiff.toFixed(4) + " seconds.");
+    console.log("That's " + (amount / timeDiff).toFixed(4) + " per second.");
 
-  // Address first-database
-  console.log("address of my first-database: " + db.address.toString())
+    /*
+    const kWhGreater30000 = docstore.query((doc) => doc["1-0:1.8.0*255"] >= 30000 )
+    console.log("greater kWh than 30000: "+ kWhGreater30000[0]._id) // QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
 
-  // Who created the db and who signed the enty
-  const identity = db.identity
-  console.log("Database creator identity _id: " + identity._id)
+    const timestampGreater = docstore.query((doc) => doc.timestamp > 1651133617620 )
+    console.log("timestamp greater than 1651133617620: "+ timestampGreater[0]._id)
 
-  // Add an entry
-  await db.put('name', 'hello')
+    const idsBetweenDates = docstore.query((doc) => doc.timestamp > 1651133617620 && doc.timestamp <= 1651146233320)
+    console.log("timestamp between 1651146233320 and 1651133617620: " + idsBetweenDates[0]._id) //QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
 
-  // OrbitDB does not automatically pin content added to IPFS. 
-  // This means that if garbage collection is triggered, 
-  // any unpinned content will be erased. 
-  await db.put('1651133617620-QmPd9GVhyMJQ7iFqzxMgwUV5fwVdWhUNNbmqmhMkv66rF2',
-    '1-0:1.8.0*255":"21678.1405 kWh',
-    { pin: true })
+    // Filter by id and timestamp
+    const betweenDatesById = docstore.query((doc) => doc.timestamp > 1651133617620 && doc.timestamp <= 1651146233320 && doc._id == "QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw")
+    console.log("timestamp between 1651146233320 and 1651133617620 and Id == QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw  " + betweenDatesById[0]._id) //QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
+    */ 
 
-
-  await db.put('1651146233320-QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw',
-    '1-0:1.8.0*255":"21678.1405 kWh',
-    { pin: true })
-
-  console.log("Put an entry.")
-
-  // get an entry
-  const value = db.get('1651133617620-QmPd9GVhyMJQ7iFqzxMgwUV5fwVdWhUNNbmqmhMkv66rF2')
-  console.log("read value: ", value)
-
-  db.close()
-
-
-// Database Type docstore
-  const docstore = await orbitdb.docstore('docstoreDB')
-
-  const hash1 = await docstore.put({ _id: 'QmPd9GVhyMJQ7iFqzxMgwUV5fwVdWhUNNbmqmhMkv66rF2', timestamp: 1651133617620, "1-0:1.8.0*255": 21678.1405 })
-  const hash2 = await docstore.put({ _id: 'QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw', timestamp: 1651146233320, "1-0:1.8.0*255": 31678.1405 })
-
-  const kWhGreater30000 = docstore.query((doc) => doc["1-0:1.8.0*255"] >= 30000 )
-  console.log("greater kWh than 30000: "+ kWhGreater30000[0]._id) // QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
-
-  const timestampGreater = docstore.query((doc) => doc.timestamp > 1651133617620 )
-  console.log("timestamp greater than 1651133617620: "+ timestampGreater[0]._id)
-
-  const idsBetweenDates = docstore.query((doc) => doc.timestamp > 1651133617620 && doc.timestamp <= 1651146233320)
-  console.log("timestamp between 1651146233320 and 1651133617620: " + idsBetweenDates[0]._id) //QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
-
-  // Filter by id and timestamp
-  const betweenDatesById = docstore.query((doc) => doc.timestamp > 1651133617620 && doc.timestamp <= 1651146233320 && doc._id == "QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw")
-  console.log("timestamp between 1651146233320 and 1651133617620 and Id == QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw  " + betweenDatesById[0]._id) //QmTruqCipSu1fyRTdEjVFqghfr54ZpJz3ZC52gFmudb7Yw
-
-  await docstore.close()
+    await docstore.close();
 }
+
 main()
